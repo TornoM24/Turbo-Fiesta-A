bcf57755afc47bf9c61e8d9a0ddbe6bf08dbf469
TornoM24 <54222500+TornoM24@users.noreply.github.com>
2021-09-23 21:47:26 -0700

    working on combat sys.

--- scenes/controller.gd
+++ scenes/controller.gd
@@ -5,9 +5,12 @@ extends Node2D
 # var a = 2
 # var b = "text"
 
+var allies = ["mc","1","2","3","4"]
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+		for i in allies:
+			
 	pass # Replace with function body.
 
 
