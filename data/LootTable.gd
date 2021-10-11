7a8ba4b36e833f36f3d09a4061c7bc4f38426f44
random-debug <vinhph003@gmail.com>
2021-10-11 12:46:54 -0700

    Containers

--- data/LootTable.gd
+++ data/LootTable.gd
@@ -1,16 +1,53 @@
 extends Node
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+var rng = RandomNumberGenerator.new()
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	rng.randomize()
+	rng.randf_range(0.0, 100.0) # Percent chance for items to drop
+	
 	pass # Replace with function body.
 
+# Loot by weight (w)
+var lootTable = [
+  {
+	item = 'balls',
+	weight = 10
+  },
+  {
+	item = 'sword',
+	weight = 10
+  },
+  {
+	item = 'shield',
+	weight = 10
+  },
+  {
+	item = 'milk',
+	weight = 10
+  },
+]
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+var total_weight = 0
+  
+func init():
+  # Calculate total weight and accumulate the weight for each item
+	for i in lootTable:
+		total_weight += i.weight
+		i.weight = total_weight
+	var loot = drop()
+	print(loot)
+
+
+func drop():
+	var rng = RandomNumberGenerator.new()
+	rng = rng.randi() % total_weight
+	for i in lootTable:
+	# if the RNG is <= item cumulated weight then drop that item
+		if rng <= i.weight && i.item != null:
+			return i
+	return null
+
