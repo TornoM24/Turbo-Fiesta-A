2b88f5ca20a9c63d2bee27e56def60e2a8fed388
random-debug <vinhph003@gmail.com>
2021-10-12 13:04:38 -0700

    Inventory Button

--- scenes/maps/Player.gd
+++ scenes/maps/Player.gd
@@ -1,33 +1,67 @@
 extends KinematicBody2D
 
-func _ready():
-	print("Ready!")
-	pass # Replace with function body.
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
 export (int) var speed = 200
+const MOVE_SPEED = 1
+
 var velocity = Vector2()
 
-# Called when the node enters the scene tree for the first time.
+onready var down=get_node ("Down")
+onready var side=get_node ("Side")
+onready var up=get_node ("Up")
 
+func _ready():
+	position = Master.partyPosition
+	speed = 200
+	side.hide()
+	up.hide()
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func hideAllBut (x):
+	down.hide()
+	side.hide()
+	up.hide()
+	x.show()
+	
 
-func getInput():
+func _process(delta):
 	velocity = Vector2()
 	if Input.is_action_pressed("right"):
-		velocity.x += 5
+		velocity.x += 1
+		hideAllBut(side)
+		side.flip_h = true
+		side.playing = true
+		side.speed_scale = 1
+
 	if Input.is_action_pressed("left"):
-		velocity.x -= 5
-	if Input.is_action_pressed("up"):
-		velocity.y -= 5
+		velocity.x -= 1
+		hideAllBut(side)
+		side.flip_h = false
+		side.playing = true
+		side.speed_scale = 1
+
 	if Input.is_action_pressed("down"):
-		velocity.y += 5
+		velocity.y += 1
+		hideAllBut(down)
+		down.playing = true
+		down.speed_scale = 1
+
+	if Input.is_action_pressed("up"):
+		velocity.y -= 1
+		hideAllBut(up)
+		up.speed_scale = 1
+	
+		
+	if (!Input.is_action_pressed("up") && !Input.is_action_pressed("down") && !Input.is_action_pressed("left") && !Input.is_action_pressed("right")):
+		down.speed_scale = 0.25
+		up.speed_scale = 0.25
+		side.speed_scale = 0.25
+
 	velocity = velocity.normalized() * speed
+	velocity = move_and_slide(velocity)
+	Master.partyPosition = global_position
+
+func _on_Start_pressed():
+	show()
+	pass # Replace with function body.
 
 
 
