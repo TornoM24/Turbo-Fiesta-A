3615e53672a3528a145a40697a6b14933fbfef54
TornoM24 <54222500+TornoM24@users.noreply.github.com>
2021-10-18 16:25:05 -0700

    mainmenu

--- scenes/credits.gd
+++ scenes/credits.gd
@@ -10,24 +10,23 @@ extends Button
 func _ready():
 	var button = self
 	button.connect("pressed", self, "_button_pressed")
+	button.connect("pressed", self, "_button_pressed")
 	button.connect("mouse_entered", self, "_mouse_entered")
 	button.connect("mouse_exited", self, "_mouse_exited")
 	oPos = self.rect_position
 
 func _button_pressed():
-	print("go")
-	Master.new_game()
-	Master.load_game()
-	get_node("/root/Global").goto_scene("res://scenes/maps/hub.tscn")
+	get_parent().get_node("creditpanel").visible = !get_parent().get_node("creditpanel").visible
+
 var oPos
 func _mouse_entered():
 	var tween = get_parent().get_node("Tween")
 	tween.interpolate_property(self, "rect_size",
 			Vector2(256, 48), Vector2(320, 48), 0.1,
-			Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
+			Tween.TRANS_QUAD, Tween.EASE_IN_OUT)
 	tween.interpolate_property(self, "rect_position",
 			oPos, Vector2 (oPos.x - 64, oPos.y), 0.1,
-			Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
+			Tween.TRANS_QUAD, Tween.EASE_IN_OUT)
 	tween.start()
 	pass
 
--- scenes/loadgame.gd
+++ scenes/loadgame.gd
@@ -10,8 +10,35 @@ extends Button
 func _ready():
 	var button = self
 	button.connect("pressed", self, "_button_pressed")
+	button.connect("pressed", self, "_button_pressed")
+	button.connect("mouse_entered", self, "_mouse_entered")
+	button.connect("mouse_exited", self, "_mouse_exited")
+	oPos = self.rect_position
 
 func _button_pressed():
 	print("loadgo")
 	Master.load_game()
 	get_node("/root/Global").goto_scene("res://scenes/maps/hub.tscn")
+
+var oPos
+func _mouse_entered():
+	var tween = get_parent().get_node("Tween")
+	tween.interpolate_property(self, "rect_size",
+			Vector2(256, 48), Vector2(320, 48), 0.1,
+			Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
+	tween.interpolate_property(self, "rect_position",
+			oPos, Vector2 (oPos.x - 64, oPos.y), 0.1,
+			Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
+	tween.start()
+	pass
+
+func _mouse_exited():
+	var tween = get_parent().get_node("Tween")
+	tween.interpolate_property(self, "rect_size",
+			 Vector2(320, 48), Vector2(256, 48), 0.1,
+			Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
+	tween.interpolate_property(self, "rect_position",
+			Vector2 (oPos.x - 64, oPos.y), oPos, 0.1,
+			Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
+	tween.start()
+	pass
