09c48c3c1b3556bb349bb5432f5c2cdf87297ea2
random-debug <vinhph003@gmail.com>
2021-11-15 20:50:48 -0800

    goodluck :)

--- scenes/detection.gd
+++ scenes/detection.gd
@@ -1,22 +1,2 @@
 extends Sprite
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	match state:
-		"Rest":
-			pass
-		"Follow":
-			pass
-		"Search":
-			pass
